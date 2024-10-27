3dbfa62986f5b668cfad4c733f09dcde2250e41b
Pikachu-345 <vishal2patel4@gmail.com>
2024-10-28 03:03:51 +0530

    added UI for admin dashboard

--- Frontend/src/pages/Dashboard/AdminDashboard/masterDashboard.jsx
+++ Frontend/src/pages/Dashboard/AdminDashboard/masterDashboard.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Bell } from "lucide-react"; 
+import { DashboardCard } from "../../../components/AdminDashboard/DashboardCard";
 
 function MasterDashboard() {
   return (
-    <div>
-      Master Dashboard
+  <>
+    <div className="absolute w-12 h-12 bg-blue-400 flex items-center justify-center rounded-full m-2 left-[1450px]">
+      <Bell className="h-6 w-6 text-gray-700 cursor-pointer hover:text-gray-900 " />
     </div>
-  )
+
+    <div className="h-lvh flex flex-col items-center justify-center bg-[#2d2d42]">
+      <div>
+        <h1 className="font-bold text-4xl mb-10 text-white">Admin Dashboard</h1>
+      </div>
+      <div className="grid grid-cols-2 gap-6 w-full max-w-4xl p-4">
+        <DashboardCard title="Assign Vendors" />
+        <DashboardCard title="View Orders" />
+        <DashboardCard title="Edit Customers & Vendors" />
+        <DashboardCard title="Add More Admins" />
+      </div>
+    </div>
+  </>
+  );
 }
 
-export default MasterDashboard
+
+export default MasterDashboard;
