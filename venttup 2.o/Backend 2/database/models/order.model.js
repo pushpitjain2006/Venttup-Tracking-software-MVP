e0b3baf9f78db97ff8430df25722f8657a3fdc01
Pushpit jain <pj325284@gmail.com>
2024-10-29 11:51:01 +0530

    Vendor backend (except data sheet upload)

--- venttup 2.o/Backend 2/database/models/order.model.js
+++ venttup 2.o/Backend 2/database/models/order.model.js
@@ -1,43 +1,5 @@
 import mongoose from "mongoose";
-
-function currentStatusArray(OrderType) {
-  if (OrderType === "localization") {
-    return [
-      "Vendor Selection",
-      "Gate 0",
-      "Gate 1",
-      "Gate 2",
-      "Gate 3",
-      "Gate 4",
-      "Gate 5",
-      "Gate 6",
-      "Gate 7",
-    ];
-  } else if (OrderType === "contract_manufacturing") {
-    return [
-      "Vendor Selection",
-      "PO Release",
-      "Drawing Submission",
-      "Drawing Approval",
-      "Manufacturing clearance",
-      "Inspection",
-      "Dispatch Clearance",
-      "Material Receipt",
-      "GRN",
-    ];
-  } else if (OrderType === "supply_chain") {
-    return [
-      "Vendor Selection",
-      "PO Release",
-      "Drawing / Data Sheet Verification",
-      "Manufacturing clearance",
-      "Inspection",
-      "Dispatch Clearance",
-      "Material Receipt",
-      "GRN",
-    ];
-  }
-}
+import orderStatuses from "../../config/orderStatusConfig.js";
 
 const orderSchema = new mongoose.Schema(
   {
@@ -61,13 +23,17 @@ const orderSchema = new mongoose.Schema(
     },
     currentStatus: {
       type: String,
-      enum: currentStatusArray(this.orderType),
+      enum: orderStatuses[this.orderType],
       default: "Vendor Selection",
     },
     vendorId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Vendor",
       default: null,
+    },
+    adminApproval: {
+      type: Boolean,
+      default: true,
     },
   },
   {
