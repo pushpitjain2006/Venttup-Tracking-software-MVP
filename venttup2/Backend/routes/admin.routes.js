87da5b0f786743f32cb672a6318f4e28e20e3f24
Pushpit jain <pj325284@gmail.com>
2024-10-29 15:06:52 +0530

    minor bug fix

--- venttup2/Backend/routes/admin.routes.js
+++ venttup2/Backend/routes/admin.routes.js
@@ -2,13 +2,13 @@ import express from "express";
 import {
   LoginAdmin,
   LogoutAdmin,
-  ApproveOrders,
-  GetAvailableVendors,
+  // ApproveOrders,
+  // GetAvailableVendors,
   AssignVendors,
   ViewUsers,
   SignupAdmin,
   deleteUsers,
-  orderUpload
+  orderUpload,
 } from "../controllers/admin.controller.js";
 // import { ViewOrders, TrackOrders } from "../controllers/order.controller.js";
 import protectedRoute from "../middlewares/ProtectedRoute.js";
@@ -17,15 +17,15 @@ import isAdmin from "../middlewares/isAdmin.js";
 const router = express.Router();
 
 router.post("/login", LoginAdmin); //Working
-router.get("/logout",protectedRoute,isAdmin, LogoutAdmin); //Working
+router.get("/logout", protectedRoute, isAdmin, LogoutAdmin); //Working
 router.post("/signup", SignupAdmin);
-router.post("/approve-orders",protectedRoute,isAdmin, ApproveOrders);  //Working
-router.get("/get-available-vendors",protectedRoute,isAdmin, GetAvailableVendors); //Working
-router.post("/assign-vendors",protectedRoute,isAdmin, AssignVendors); //Working
-router.get("/view-users",protectedRoute,isAdmin, ViewUsers); //Working
+// router.post("/approve-orders",protectedRoute,isAdmin, ApproveOrders);  //Working
+// router.get("/get-available-vendors",protectedRoute,isAdmin, GetAvailableVendors); //Working
+router.post("/assign-vendors", protectedRoute, isAdmin, AssignVendors); //Working
+router.get("/view-users", protectedRoute, isAdmin, ViewUsers); //Working
 // router.get("/view-orders",protectedRoute,isAdmin, ViewOrders); //Working (jo jo route comment hai unhe dekhna hai backend crash karra hai inhe uncomment karne par..)
 // router.get("/track-orders",protectedRoute,isAdmin, TrackOrders);  //Working
-router.delete("/delete-user",protectedRoute,isAdmin,deleteUsers);
-router.post("/upload-order",protectedRoute,isAdmin,orderUpload);
+router.delete("/delete-user", protectedRoute, isAdmin, deleteUsers);
+router.post("/upload-order", protectedRoute, isAdmin, orderUpload);
 
 export default router;
