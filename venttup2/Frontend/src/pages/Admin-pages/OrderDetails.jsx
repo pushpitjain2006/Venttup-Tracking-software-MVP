face4eac2e742196d214aca44a5c804eaa11b666
Pikachu-345 <vishal2patel4@gmail.com>
2024-10-30 01:56:53 +0530

    minor changes

--- venttup2/Frontend/src/pages/Admin-pages/OrderDetails.jsx
+++ venttup2/Frontend/src/pages/Admin-pages/OrderDetails.jsx
@@ -142,9 +142,17 @@ const OrderDetails = () => {
               </div>
               <div className="flex justify-end items-center">
                 <button
-                  className="m-4 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                  className="m-4 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                  onClick={()=>console.log("to code")}
+                >
+                  Edit Details
+                </button>
+                <button
+                  className="m-4 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                   onClick={handleDeleteOrder}
-                >Delete Order</button>
+                >
+                  Delete Order
+                </button>
               </div>
             </div>
           )
