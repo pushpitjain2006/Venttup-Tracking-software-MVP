92e985e473b010451fc7558374d076d50feca8cc
Pushpit jain <pj325284@gmail.com>
2024-11-08 23:58:07 +0530

    Add Vercel Analytics

--- venttup2/Frontend/src/main.jsx
+++ venttup2/Frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./css/tailwind.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <>
@@ -11,5 +12,6 @@ createRoot(document.getElementById("root")).render(
       <App />
     </AuthProvider>
     <ToastContainer />
+    <Analytics />
   </>
 );
