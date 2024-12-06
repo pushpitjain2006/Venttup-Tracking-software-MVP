236de0f6075f0679f62fc17d031576a5c7fd8c3b
Pushpit jain <pj325284@gmail.com>
2024-12-06 13:08:37 +0530

    Refactoring

--- venttup1/Backend/middlewares/isAdmin.js
+++
@@ -1,8 +0,0 @@
-const isAdmin = (req, res, next) => {
-  if (req.body.LoggedInUserType !== 'admin') {
-    return res.status(403).json({ message: "Access denied: Admins only" });
-  }
-  next();
-};
-
-export default isAdmin;
